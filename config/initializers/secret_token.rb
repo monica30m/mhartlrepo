ac37a50ad5f869019412177af50f118bf0cf5e89
Monica Mishra <monica.mishra@sendgrid.com>
2013-06-23 12:20:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhartlApp::Application.config.secret_token = 'bb9e5b52b1c9b97e32c27bc879e69c79d8967e549709d4f2eee65e7a1bc8cc05860aaba3b2b58da7ff6ad5f83298b21cf78d6c7b6e812ae37793107b1702bdb5'
