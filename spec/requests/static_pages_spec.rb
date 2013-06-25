30d90b843b7c72e772bd15401563c7a9076fd4f2
Monica Mishra <monica.mishra@sendgrid.com>
2013-06-25 08:53:26 -0600

    Add a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe "Static pages" do
 
-let (:base) {"Ruby on Rails | "}
+let (:base) {"Ruby on Rails"}
 
   describe "Home page" do
 
@@ -10,13 +10,18 @@ let (:base) {"Ruby on Rails | "}
       page.should have_content('Sample App')
 	end 
 
-	it "should have the home title" do
-  		visit '/static_pages/home'
-  		page.should have_selector('title', 
-  			:text =>  base+"Home")
+	it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => base)
+	end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => ' | Home')
 	end
 	
-end
+  end
 
    describe "Help page" do
 
@@ -28,7 +33,7 @@ end
 		it "should have the help title" do
   			visit '/static_pages/help'
   			page.should have_selector('title', 
-  				:text => base+"Help")
+  				:text => base+' | Help')
 		end
 
  	end
@@ -42,7 +47,7 @@ end
    		it "should have the about us title" do
   			visit '/static_pages/about'
   			page.should have_selector('title', 
-  				:text => base+"About Us")
+  				:text => base+' | About Us')
 		end
    	end
 
@@ -55,7 +60,7 @@ end
    		it "should have the contact title" do
   			visit '/static_pages/contact'
   			page.should have_selector('title', 
-  				:text => base+"Contact")
+  				:text => base+' | Contact')
 		end
    	end
 end
