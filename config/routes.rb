495c3a4e56d48402995458ad585af27573dc220a
Monica Mishra <monica.mishra@sendgrid.com>
2013-06-30 22:30:36 -0600

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ MhartlApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signin',  to: 'sessions#new'
+  match 'signout',  to: 'sessions#destroy', via: :delete
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
